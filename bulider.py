728ad66a9fe1546f1a0a919e57f083576119bbf5
karmazynowyksiaze <86152886+karmazynowyksiaze@users.noreply.github.com>
2023-06-17 02:38:37 +0200

    Update bulider.py

--- bulider.py
+++ bulider.py
@@ -43,7 +43,39 @@ class ConcreteBuilderH(Builder):
     def pizzaTopping(self) -> None:
         self._product.add("ham + pineapple")
 
+class ConcreteBuilderS(Builder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = HawaiianPizza()
+
+    @property
+    def product(self) -> SpicyPizza:
+        product = self._product
+        self.reset()
+        return product
+
+    def pizzaDough(self) -> None:
+        self._product.add("thin")
+
+    def pizzaSauce(self) -> None:
+        self._product.add("hot")
+
+    def pizzaTopping(self) -> None:
+        self._product.add("salami + pepperoni")
+
 class HawaiianPizza():
+    def __init__(self) -> None:
+        self.elements = []
+
+    def add(self, part: Any) -> None:
+        self.elements.append(part)
+
+    def list_elements(self) -> None:
+        print(f"Pizza elements: {', '.join(self.elements)}", end="")
+
+class SpicyPizza():
     def __init__(self) -> None:
         self.elements = []
 
@@ -74,18 +106,43 @@ class Waiter():
         self.builder.pizzaTopping()
 
 if __name__ == "__main__":
+
+
+    """
     waiter = Waiter()
-    builder = ConcreteBuilderH()
-    waiter.builder = builder
+    builderS = ConcreteBuilderS()
+    waiter.builder = builderS
+    """
+    print("\t\tDesignPattern_Builder\n")
 
-    print("\t\tDesignPattern_Builder")
-    print("\nHawaiian Pizza: ")
-    waiter.build_full_pizza()
-    builder.product.list_elements()
+    pizzaType =input("{h}HawaiianPizza {s}SpicyPizza  ")
+    if pizzaType == "h":
+        waiter = Waiter()
+        builder = ConcreteBuilderH()
+        waiter.builder = builder
 
-    print("\n")
+        print("\nHawaiian Pizza ")
+        waiter.build_full_pizza()
+        builder.product.list_elements()
+        print("\n")
 
-    print("Custom Hawaiian Pizza elements: ")
-    builder.pizzaDough()
-    builder.pizzaSauce()
-    builder.product.list_elements()
+        print("Custom Hawaiian Pizza elements ")
+        builder.pizzaDough()
+        builder.pizzaSauce()
+        builder.product.list_elements()
+
+    if pizzaType == "s":
+        waiter = Waiter()
+        builder = ConcreteBuilderS()
+        waiter.builder = builder
+
+        print("\nSpicy Pizza ")
+        waiter.build_full_pizza()
+        builder.product.list_elements()
+
+        print("\n")
+
+        print("Custom Spicy Pizza elements ")
+        builder.pizzaDough()
+        builder.pizzaSauce()
+        builder.product.list_elements()
